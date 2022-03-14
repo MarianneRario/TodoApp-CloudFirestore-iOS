384b40ec83569e954a2784a03a4f7cfd1c0a1e01
Marianne Rario <mariannecastillorario27@gmail.com>
2022-03-14 19:13:04 +0800

    initial file setup

---
+++ TodoApp/TodoAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodoAppApp.swift
+//  TodoApp
+//
+//  Created by Mapletree on 3/14/22.
+//
+
+import SwiftUI
+
+@main
+struct TodoAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
