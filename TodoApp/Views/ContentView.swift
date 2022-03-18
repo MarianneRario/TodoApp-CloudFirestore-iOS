8211a7a4fec3d393917870769d588c56e3d35820
Marianne Rario <mariannecastillorario27@gmail.com>
2022-03-18 18:03:50 +0800

    create another view for notes

--- TodoApp/Views/ContentView.swift
+++ TodoApp/Views/ContentView.swift
@@ -9,7 +9,7 @@ import SwiftUI
 import Firebase
 
 // Customize button style
-struct AddTodoBtn: ButtonStyle {
+struct AddTodoBtnDesign: ButtonStyle {
     func makeBody(configuration: Configuration) -> some View {
         configuration.label
         .padding(10)
@@ -26,23 +26,32 @@ struct ContentView: View {
     // Initialize ViewModel
     @ObservedObject var model = ViewModel()
     
-    // Variable to handle name and notes
-    @State var name: String = ""
-    @State var notes: String = ""
-    
     var body: some View {
         NavigationView {
             VStack {
                 List (model.list){ item in
+                    
                     HStack {
-                        NavigationLink(destination: Text(item.notes)) {
+                        // Go to the second screen and pass param
+                        // model (expected param in 2nd screen): model (local variable here)
+                        
+                        ZStack(alignment: .leading) {
                             Text(item.name)
+                                
+                            NavigationLink(destination: NotesView()) {
+                                EmptyView()
+                                    
+                            }
+                            
+                            .buttonStyle(PlainButtonStyle())
                         }
                         
+
                         Spacer()
                         
                         Button {
                             print("hello")
+                            print(type(of: item.notes))
                         } label: {
                             Image(systemName: "pencil")
                         }
@@ -57,36 +66,13 @@ struct ContentView: View {
                         }
                         .buttonStyle(BorderlessButtonStyle())
                         .foregroundColor(.red)
-                    }
+                    } 
                 }
                 
                 Divider()
                 
                 // Add button
-                VStack(spacing: 5) {
-                    // $ -> for binding purposes
-                    // This textfields will be used for adding data in db
-                    TextField("Name", text: $name)
-                        .textFieldStyle(RoundedBorderTextFieldStyle())
-                    TextField("Notes", text: $notes)
-                        .textFieldStyle(RoundedBorderTextFieldStyle())
-                        .padding(.bottom)
-                    
-                    Button {
-                        // Call add data
-                        model.addData(name: name, notes: notes)
-                        
-                        // Clear the text fields
-                        name = ""
-                        notes = ""
-                    } label: {
-                        Text("Add todo item")
-                    }
-                    .buttonStyle(AddTodoBtn())
-                } .padding()
-                
-                // insert navigation properties here
-                    .navigationTitle("Todo List")
+                AddTodoBtn(model: model)
             }
         }
     }
@@ -101,5 +87,41 @@ struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
 
+    }
+}
+
+struct AddTodoBtn: View {
+    
+    // Variable to handle name and notes
+    @State var name: String = ""
+    @State var notes: String = ""
+    let model: ViewModel
+    
+    
+    var body: some View {
+        VStack(spacing: 5) {
+            // $ -> for binding purposes
+            // This textfields will be used for adding data in db
+            TextField("Name", text: $name)
+                .textFieldStyle(RoundedBorderTextFieldStyle())
+            TextField("Notes", text: $notes)
+                .textFieldStyle(RoundedBorderTextFieldStyle())
+                .padding(.bottom)
+            
+            Button {
+                // Call add data
+                model.addData(name: name, notes: notes)
+                
+                // Clear the text fields
+                name = ""
+                notes = ""
+            } label: {
+                Text("Add todo item")
+            }
+            .buttonStyle(AddTodoBtnDesign())
+        } .padding()
+        
+        // insert navigation properties here
+            .navigationTitle("Todo List")
     }
 }
---
+++ TodoApp/Views/NotesView.swift
@@ -0,0 +1,23 @@
+//
+//  NotesView.swift
+//  TodoApp
+//
+//  Created by Mapletree on 3/15/22.
+//
+
+import SwiftUI
+
+struct NotesView: View {
+    // param from main content view
+    var body: some View {
+        
+        Text("item.notes")
+    }
+}
+
+struct NotesView_Previews: PreviewProvider {
+    
+    static var previews: some View {
+        NotesView()
+    }
+}
